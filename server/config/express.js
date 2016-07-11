0a8db735e6b348b630bf77222b82d1f304cb5940
Blake Hagen <blakehagen@gmail.com>
2016-07-10 22:16:00 -0600

    added morgan to server config

--- server/config/express.js
+++ server/config/express.js
@@ -5,10 +5,12 @@ const express    = require('express');
 const session    = require('express-session');
 const bodyParser = require('body-parser');
 const cors       = require('cors');
+const logger     = require('morgan');
 
 module.exports = () => {
   const app = express();
 
+  app.use(logger('dev'));
   app.use(cors());
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(session({
